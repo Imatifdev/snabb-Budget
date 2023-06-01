4d6c926c9ab050b51f522ed3797bec472bf88e13
Abdullah Ayaz <106861398+abd529@users.noreply.github.com>
2023-06-01 10:36:38 +0500

    updated tab

--- lib/Screens/transactions_screen.dart
+++ lib/Screens/transactions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:material_segmented_control/material_segmented_control.dart';
 
 class TransactionsScreen extends StatefulWidget {
@@ -9,9 +10,11 @@ class TransactionsScreen extends StatefulWidget {
 class _TransactionsScreenState extends State<TransactionsScreen>{
 int _currentSelection = 0;
 final Map<int, Widget> _children = {
-  0: const Text('Daily'),
-  1: const Padding(padding: EdgeInsets.all(8) ,child:Text('Monthly')),
-  2: const Text('Yearly'),
+  0: Text('Daily',style: GoogleFonts.montserrat(),),
+  1:  Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20),
+    child:Text('Monthly',style: GoogleFonts.montserrat(),)),
+  2:  Text('Yearly',style: GoogleFonts.montserrat(),),
 };
   @override
   void initState() {
@@ -26,36 +29,41 @@ final Map<int, Widget> _children = {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text(
-          'My App',
-          style: TextStyle(color: Colors.black),
-        ),
-       
+      body: SafeArea(
+        child: SizedBox(width: double.infinity,
+        child: Column(children: [
+         Card(
+          child: SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(child: IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_back_rounded)),) ,
+                const Text("Transactions", style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                IconButton(onPressed: (){}, icon: ImageIcon(const AssetImage("assets/images/search.png"),size: 40,color: Theme.of(context).primaryColor) )],
+            ),
+          )),
+          MaterialSegmentedControl(
+            verticalOffset: 12,
+          children: _children,
+          selectionIndex: _currentSelection,
+          borderColor: Theme.of(context).primaryColor ,
+          selectedColor: Theme.of(context).primaryColor,
+          unselectedColor: Colors.white,
+          selectedTextStyle: const TextStyle(color: Colors.white),
+          unselectedTextStyle: TextStyle(color: Theme.of(context).primaryColor),
+          borderWidth: 0.7,
+          borderRadius: 32.0,
+          disabledChildren: [3],
+          onSegmentTapped: (index) {
+        setState(() {
+          _currentSelection = index;
+        });
+          },
       ),
-      body: SizedBox(width: double.infinity,
-      child: Column(children: [
-        MaterialSegmentedControl(
-    children: _children,
-    selectionIndex: _currentSelection,
-    borderColor: Colors.grey,
-    selectedColor: Colors.redAccent,
-    unselectedColor: Colors.white,
-    selectedTextStyle: const TextStyle(color: Colors.white),
-    unselectedTextStyle: const TextStyle(color: Colors.redAccent),
-    borderWidth: 0.7,
-    borderRadius: 32.0,
-    disabledChildren: [3],
-    onSegmentTapped: (index) {
-      setState(() {
-        _currentSelection = index;
-      });
-    },
-),
-_children[_currentSelection] as Widget
-      ]),
+      _children[_currentSelection] as Widget
+        ]),
+        ),
       )
     );
   }
