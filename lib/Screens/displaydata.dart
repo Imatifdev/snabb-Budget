2882e3bb5e2c37173444e94c0a6a6b598d8fc557
Muhammad Atif Pervaiz <matifpervaizofficial@gmail.com>
2023-06-07 01:21:40 +0500

    update balance

--- lib/Screens/displaydata.dart
+++ lib/Screens/displaydata.dart
@@ -6,38 +6,57 @@ class DisplayIncomeScreen extends StatelessWidget {
   final List<IncomeData> incomeList;
 
   const DisplayIncomeScreen(this.incomeList);
+  double calculateTotalAmount() {
+    double totalAmount = 0;
+    for (IncomeData expense in incomeList) {
+      totalAmount += expense.amount;
+    }
+    return totalAmount;
+  }
 
   @override
   Widget build(BuildContext context) {
+    double totalAmount = calculateTotalAmount();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Income Details'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: incomeList.length,
-            itemBuilder: (context, index) {
-              IncomeData income = incomeList[index];
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Total Amount:'),
+              trailing: Text(totalAmount.toStringAsFixed(2)),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: incomeList.length,
+                  itemBuilder: (context, index) {
+                    IncomeData income = incomeList[index];
 
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    income.imageurl,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Text('Name: ${income.name}'),
-                  SizedBox(height: 8.0),
-                  Text('Amount: ${income.amount}'),
-                  SizedBox(height: 8.0),
-                  SizedBox(height: 8.0),
-                  Text('Date: ${DateFormat.yMMMd().format(income.date)}'),
-                  Text('Date: ${(income.time)}'),
-                ],
-              );
-            }),
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          income.imageurl,
+                          width: 100,
+                          height: 100,
+                        ),
+                        Text('Name: ${income.name}'),
+                        SizedBox(height: 8.0),
+                        Text('Amount: ${income.amount}'),
+                        SizedBox(height: 8.0),
+                        SizedBox(height: 8.0),
+                        Text('Date: ${DateFormat.yMMMd().format(income.date)}'),
+                        Text('Date: ${(income.time)}'),
+                      ],
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
