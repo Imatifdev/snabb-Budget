46758bf556d5eff2977ac5afa767ba27a1af5a45
Muhammad Atif Pervaiz <matifpervaizofficial@gmail.com>
2023-07-07 02:53:12 +0500

    chart

--- lib/Screens/setting_screen.dart
+++ lib/Screens/setting_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction ;
+import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pie_chart/pie_chart.dart';
 import 'package:snabbudget/Screens/theme_screen.dart';
 import 'package:snabbudget/utils/custom_drawer.dart';
 import 'package:syncfusion_flutter_xlsio/xlsio.dart' hide Column hide Row;
@@ -10,6 +11,7 @@ import 'package:open_file/open_file.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:pdf/widgets.dart' as pdfWidgets;
+import 'package:syncfusion_officechart/officechart.dart';
 
 import '../models/currency_controller.dart';
 import '../models/transaction.dart';
@@ -43,69 +45,139 @@ class _SettingScreenState extends State<SettingScreen> {
     transactions = transactionData.transactions;
   }
 
-Future<void> showDeleteConfirmationDialog(BuildContext context) async {
-  return showDialog<void>(
-    context: context,
-    barrierDismissible: false,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('Delete All Data'),
-        content: const Text('Are you sure you want to delete all your data? This action cannot be undone.'),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Cancel'),
-            onPressed: () {
-              Navigator.of(context).pop(); // Close the dialog
-            },
-          ),
-          TextButton(
-            child: const Text('Delete'),
-            onPressed: () async{
-              await FirebaseFirestore.instance.collection("UserTransactions").doc(userId).collection("transactions").get().then((snapshot) {
-  for (DocumentSnapshot ds in snapshot.docs){
-    ds.reference.delete();
+  Future<void> showDeleteConfirmationDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete All Data'),
+          content: const Text(
+              'Are you sure you want to delete all your data? This action cannot be undone.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                await FirebaseFirestore.instance
+                    .collection("UserTransactions")
+                    .doc(userId)
+                    .collection("transactions")
+                    .get()
+                    .then((snapshot) {
+                  for (DocumentSnapshot ds in snapshot.docs) {
+                    ds.reference.delete();
+                  }
+                });
+                await FirebaseFirestore.instance
+                    .collection("UserTransactions")
+                    .doc(userId)
+                    .collection("data")
+                    .doc("userData")
+                    .update({
+                  "balance": 0,
+                  "currency": "\$",
+                });
+                await FirebaseFirestore.instance
+                    .collection("UserTransactions")
+                    .doc(userId)
+                    .collection("SchedualTrsanactions")
+                    .get()
+                    .then((snapshot) {
+                  for (DocumentSnapshot ds in snapshot.docs) {
+                    ds.reference.delete();
+                  }
+                });
+                await FirebaseFirestore.instance
+                    .collection("UserTransactions")
+                    .doc(userId)
+                    .collection("Accounts")
+                    .get()
+                    .then((snapshot) {
+                  for (DocumentSnapshot ds in snapshot.docs) {
+                    if (ds.id == "snabbWallet") {
+                      FirebaseFirestore.instance
+                          .collection("UserTransactions")
+                          .doc(userId)
+                          .collection("Accounts")
+                          .doc("snabbWallet")
+                          .update({'amount': 0});
+                    } else {
+                      ds.reference.delete();
+                    }
+                  }
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
-});
-              await FirebaseFirestore.instance.collection("UserTransactions").doc(userId).collection("data").doc("userData").update(
-                {
-                  "balance":0,
-                  "currency":"\$",
-                }
-              );
-              await FirebaseFirestore.instance.collection("UserTransactions").doc(userId).collection("SchedualTrsanactions").get().then((snapshot) {
-              for (DocumentSnapshot ds in snapshot.docs){
-              ds.reference.delete();
-              }
-          });
-              await FirebaseFirestore.instance.collection("UserTransactions").doc(userId).collection("Accounts").get().then((snapshot) {
-              for (DocumentSnapshot ds in snapshot.docs){
-              if(ds.id=="snabbWallet"){
-               FirebaseFirestore.instance.collection("UserTransactions")
-                                .doc(userId).collection("Accounts").doc("snabbWallet")
-                                .update({'amount': 0});
-              }else{
-              ds.reference.delete();
-              }
-              }
-          });
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
-  );
-}
-
 
   Future<void> createExcel(List<Transaction> transactions) async {
     final Workbook workbook = Workbook();
     final Worksheet sheet = workbook.worksheets[0];
+    Style globalStyle = workbook.styles.add('style');
+    globalStyle.backColor = '#37D8E9';
 
     // Set header "Snabb Budget"
-    sheet.getRangeByName('A1').setText('Snabb Budget!');
+    sheet.getRangeByName('B1').setText('Snabb Budget!');
 
-    // Set column headers
+    sheet.getRangeByName('B1').columnWidth = 25;
+    sheet.getRangeByName('B1').rowHeight = 80;
+
+    final Range rangeA1C1 = sheet.getRangeByName('A1:D1');
+    rangeA1C1.cellStyle.backColor = '#87CEEB';
+    final Style cellStyle = sheet.getRangeByName('B1').cellStyle;
+
+    cellStyle.backColor = '#87CEEB';
+    cellStyle.fontSize = 28;
+
+    // Replace with your desired color
+    sheet.getRangeByName('A1').cellStyle = cellStyle;
+    final Range rangeA2 = sheet.getRangeByIndex(2, 1);
+    rangeA2.setText('Serial Number');
+    rangeA2.cellStyle
+      ..backColor = '#FFFF00' // Yellow background color
+      ..hAlign = HAlignType.center;
+    sheet.getRangeByIndex(1, 1).columnWidth = 15; // Increase width of column 1
+
+    final Range rangeB2 = sheet.getRangeByIndex(2, 2);
+    rangeB2.setText('Date');
+    rangeB2.cellStyle
+      ..backColor = '#FFFF00' // Yellow background color
+      ..hAlign = HAlignType.center;
+    sheet.getRangeByIndex(1, 2).columnWidth = 10; // Increase width of column 2
+
+    final Range rangeC2 = sheet.getRangeByIndex(2, 3);
+    rangeC2.setText('Transaction Category');
+    rangeC2.cellStyle
+      ..backColor = '#FFFF00' // Yellow background color
+      ..hAlign = HAlignType.center;
+    sheet.getRangeByIndex(1, 3).columnWidth = 18; // Increase width of column 3
+
+    final Range rangeD2 = sheet.getRangeByIndex(2, 4);
+    rangeD2.setText('Amount');
+    rangeD2.cellStyle
+      ..backColor = '#FFFF00' // Yellow background color
+      ..hAlign = HAlignType.center;
+    sheet.getRangeByIndex(1, 4).columnWidth = 8; // Increase width of column 4
+
+    final Range rangeE2 = sheet.getRangeByIndex(2, 5);
+    rangeE2.setText('Note');
+    rangeE2.cellStyle
+      ..backColor = '#FFFF00' // Yellow background color
+      ..hAlign = HAlignType.center;
+    sheet.getRangeByIndex(1, 5).columnWidth = 7; // Increase width of column 4
+
+    sheet.getRangeByIndex(1, 5).columnWidth = 30; // Increase width of column 5
     sheet.getRangeByIndex(2, 1).setText('Serial Number');
     sheet.getRangeByIndex(2, 2).setText('Date');
     sheet.getRangeByIndex(2, 3).setText('Transaction Category');
@@ -137,20 +209,172 @@ Future<void> showDeleteConfirmationDialog(BuildContext context) async {
       }
     }
 
+// Create an instance of chart collection.
+    final ChartCollection charts = ChartCollection(sheet);
+
+// Add the chart.
+    final Chart chart = charts.add();
+
+// Set Chart Type.
+    chart.chartType = ExcelChartType.pie;
+
+// Set data range in the worksheet.
+    chart.dataRange = sheet.getRangeByName('C3:D${transactions.length + 2}');
+
+// Set chart position.
+    chart.topRow = transactions.length + 5;
+    chart.bottomRow = transactions.length + 25;
+    chart.leftColumn = 1;
+    chart.rightColumn = 8;
+
+// Set chart title.
+    chart.chartTitle = 'Transaction Categories';
+
+// Set chart title font properties.
+
+// Set chart data labels.
+    //chart.hasTitle = true;
+
+// Set chart legend.
+    chart.hasLegend = true;
+    chart.legend?.position = ExcelLegendPosition.right;
+
+// Set charts to worksheet.
+    sheet.charts = charts;
     final List<int> bytes = workbook.saveAsStream();
     workbook.dispose();
 
-    final String path = (await getApplicationSupportDirectory()).path;
-    final String fileName = '$path/Output.xlsx';
-    final File file = File(fileName);
+    final directory = await getApplicationSupportDirectory();
+    final file = File('${directory.path}/Output.xlsx');
     await file.writeAsBytes(bytes, flush: true);
-    OpenFile.open(fileName);
+
+    OpenFile.open(file.path);
   }
+
+  Future<void> createExcelWithChart() async {
+    // Create a new Excel document.
+    final Workbook workbook = Workbook();
+    // Accessing worksheet via index.
+    final Worksheet sheet = workbook.worksheets[0];
+
+    // Setting value in the cell.
+    sheet.getRangeByName('A1').setText('John');
+    sheet.getRangeByName('A2').setText('Amy');
+    sheet.getRangeByName('A3').setText('Jack');
+    sheet.getRangeByName('A4').setText('Tiya');
+    sheet.getRangeByName('B1').setNumber(10);
+    sheet.getRangeByName('B2').setNumber(12);
+    sheet.getRangeByName('B3').setNumber(20);
+    sheet.getRangeByName('B4').setNumber(21);
+
+    // Create an instance of chart collection.
+    final ChartCollection charts = ChartCollection(sheet);
+
+    // Add the chart.
+    final Chart chart = charts.add();
+
+    // Set Chart Type.
+    chart.chartType = ExcelChartType.pie3D;
+
+    // Set data range in the worksheet.
+    chart.dataRange = sheet.getRangeByName('A1:B4');
+
+    // Set chart position.
+    chart.topRow = 1;
+    chart.bottomRow = 20;
+    chart.leftColumn = 1;
+    chart.rightColumn = 8;
+
+    // Set chart title.
+    chart.chartTitle = 'Sample Chart';
+
+    // Set chart title font properties.
+
+    // Set chart data labels.
+    chart.hasTitle = true;
+
+    // Set chart legend.
+    chart.hasLegend = true;
+    chart.legend?.position = ExcelLegendPosition.bottom;
+
+    // Set charts to worksheet.
+    sheet.charts = charts;
+
+    // Save the workbook.
+    final List<int> bytes = workbook.saveAsStream();
+
+    // Dispose the workbook.
+    workbook.dispose();
+
+    // Get the application support directory.
+    final directory = await getApplicationSupportDirectory();
+
+    // Create the output file path.
+    final filePath = '${directory.path}/Chart.xlsx';
+
+    // Write the bytes to the output file.
+    await File(filePath).writeAsBytes(bytes, flush: true);
+
+    // Open the file.
+    OpenFile.open(filePath);
+  }
+
+  // Future<void> createExcel(List<Transaction> transactions) async {
+  //   final Workbook workbook = Workbook();
+  //   final Worksheet sheet = workbook.worksheets[0];
+
+  //   // Set header "Snabb Budget"
+  //   sheet.getRangeByName('A1').setText('Snabb Budget!');
+
+  //   // Set column headers
+  //   sheet.getRangeByIndex(2, 1).setText('Serial Number');
+  //   sheet.getRangeByIndex(2, 2).setText('Date');
+  //   sheet.getRangeByIndex(2, 3).setText('Transaction Category');
+  //   sheet.getRangeByIndex(2, 4).setText('Amount');
+  //   sheet.getRangeByIndex(2, 5).setText('Note');
+
+  //   // Add transaction data
+  //   for (int i = 0; i < transactions.length; i++) {
+  //     final Transaction transaction = transactions[i];
+  //     final int row = i + 3; // Starting from row 3
+
+  //     // Serial Number
+  //     sheet.getRangeByIndex(row, 1).setNumber(i + 1);
+
+  //     // Date
+  //     sheet.getRangeByIndex(row, 2).setDateTime(transaction.date);
+
+  //     // Transaction Category
+  //     sheet
+  //         .getRangeByIndex(row, 3)
+  //         .setText(transaction.category.toString().split('.').last);
+
+  //     // Amount
+  //     sheet.getRangeByIndex(row, 4).setNumber(transaction.amount.toDouble());
+
+  //     // Note (if exists)
+  //     if (transaction.name.isNotEmpty) {
+  //       sheet.getRangeByIndex(row, 5).setText(transaction.name);
+  //     }
+  //   }
+
+  //   final List<int> bytes = workbook.saveAsStream();
+  //   workbook.dispose();
+
+  //   final String path = (await getApplicationSupportDirectory()).path;
+  //   final String fileName = '$path/Output.xlsx';
+  //   final File file = File(fileName);
+  //   await file.writeAsBytes(bytes, flush: true);
+  //   OpenFile.open(fileName);
+  // }
 
   Future<void> generateExcel() async {
     // Create a new Excel document
     final Workbook workbook = Workbook();
     final Worksheet sheet = workbook.worksheets[0];
+    Style globalStyle = workbook.styles.add('style');
+
+    globalStyle.fontSize = 20;
 
     // Set column widths
     sheet.getRangeByName('A1:D1').columnWidth = 15;
@@ -227,7 +451,6 @@ Future<void> showDeleteConfirmationDialog(BuildContext context) async {
     OpenFile.open(filePath);
   }
 
-  
   Future<void> createPDF(List<Transaction> transactions) async {
     final pdf = pw.Document();
 
@@ -423,7 +646,7 @@ Future<void> showDeleteConfirmationDialog(BuildContext context) async {
                         ListTile(
                           title: const Text(
                             "Export ",
-                            style:  TextStyle(fontWeight: FontWeight.bold),
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                           subtitle: const Text("xls"),
                           trailing: const Icon(Icons.arrow_forward_ios_rounded),
